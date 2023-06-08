d59c6ee037ed099ff82c4fc13fc0874873c4139f
Amitai Cohen <71866656+korniko98@users.noreply.github.com>
2023-06-08 09:21:17 +0300

    Update VTC.py

--- VTC.py
+++ VTC.py
@@ -17,35 +17,32 @@ sheet.cell_value(0, 0)
 wbwrite = Workbook()
 sheet1 = wbwrite.add_sheet('Hashes')
 sheet1.write(0, 0, 'MD5')
-sheet1.write(0, 1, 'SHA-1')
-sheet1.write(0, 2, 'SHA-256')
-sheet1.write(0, 3, 'Detections')
-sheet1.write(0, 4, 'Total AVs looked')
+sheet1.write(0, 1, 'SHA1')
+sheet1.write(0, 2, 'SHA256')
 
 # VT API data, Do not Change or Share
-url = 'https://www.virustotal.com/vtapi/v2/file/report'
+url = 'https://www.virustotal.com/api/v3/files/'
 API_KEY = '<API KEY GOES HERE>'
 HASH = ''
 
 # Moving row by row down
 for i in range(sheet.nrows):
     HASH = (sheet.cell_value(i, 0))
-    params = {'apikey': API_KEY, 'resource': HASH}
-    response = requests.get(url, params=params)
+    vt_url = url + HASH
+    header = {'x-apikey': API_KEY}
+    response = requests.get(vt_url, headers=header)
     data = response.json()
-    md5 = data["md5"]
-    SHA1 = data["sha1"]
-    SHA256 = data["sha256"]
-    positives = data["positives"]
-    total = data["total"]
+    attr = data.get("data").get("attributes")
+    MD5 = attr.get("md5")
+    SHA1 = attr.get("sha1")
+    SHA256 = attr.get("sha256")
+    print(SHA1)
     # Writing Data to new Excel sheet
-    sheet1.write(i + 1, 0, md5)
+    sheet1.write(i + 1, 0, MD5)
     sheet1.write(i + 1, 1, SHA1)
     sheet1.write(i + 1, 2, SHA256)
-    sheet1.write(i + 1, 3, positives)
-    sheet1.write(i + 1, 4, total)
     print(i + 1, " of ", sheet.nrows, " Completed with response ",response)
-    time.sleep(16)  # VirusTotal Public API allows only 4 requests per minute, Do not change the sleep duration.
+    time.sleep(1)  # VirusTotal Public API allows only 4 requests per minute, Do not change the sleep duration.
 
 
 wbwrite.save('HashConvertedOutput.xls')
